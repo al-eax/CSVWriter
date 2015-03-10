95dcd044309611e1c0786d9ab57698facb644ef1
al-eax <hinze.alex@googlemail.com>
2015-03-10 19:56:21 +0100

    Changed tests

--- main.cpp
+++ main.cpp
@@ -5,30 +5,29 @@ using namespace std;
 void test1(){
     CSVWriter csv;
     csv << "this" << "is" << "a" << "row";
-    cout << csv << endl << "=====================" << endl;
+    cout << csv << endl;
 }
 
 void test2(){
     CSVWriter csv;
     csv.newRow() << "this" << "is" << "the" << "first" << "row";
     csv.newRow() << "this" << "is" << "the" << "second" << "row";
-    cout << csv << endl << "=====================" << endl;
+    cout << csv << endl <<;
 }
 
 void test3(){
     CSVWriter csv;
     csv.enableAutoNewRow(5);
     csv << "this" << "is" << "the" << "first" << "row" << "this" << "is" << "the" << "second" << "row";
-    cout << csv << endl << "=====================" << endl;
+    cout << csv << endl <<;
 }
 
 void test4(){
     CSVWriter csv;
     csv.newRow() << "this" << "is" << "the" << "first" << "row";
     csv.newRow() << "this" << "is" << "the" << "second" << "row";
-    cout << csv.writeToFile("foobar.csv") << endl << "=====================" << endl;
+    cout << csv.writeToFile("foobar.csv") << endl;
 }
-
 
 int main()
 {
